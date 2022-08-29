dd447c98acd5be6e8fd06160fbb6285a4c5f61e7
kalaimani-ms <108936435+kalaimani-ms@users.noreply.github.com>
2022-08-29 21:09:56 +0530

    Create scriptmaven.groovy

---
+++ scriptmaven.groovy
@@ -0,0 +1,28 @@
+def gv
+
+def Buildjar() {
+       echo 'building the maven application..'
+       sh 'mvn package'
+    }
+    
+def Buildimage() {
+       echo 'building the maven application Image..'
+       withCredentials([usernamePassword(credentialsId: 'kalaimanims-Dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+       sh 'docker images'
+       sh 'docker build -t kalaimanims/mavenapp:1.2 .'
+       sh 'docker images'
+       sh 'echo $PASS | docker login -u $USER --password-stdin'
+       sh 'docker push kalaimanims/mavenapp:1.2'
+       sh 'docker images'
+    }
+
+def Testapp() {
+        echo 'Testing the application..'
+        sh 'mvn test'
+    }
+
+def Deployapp() {
+       echo 'deploying the application'
+    }
+
+return this
