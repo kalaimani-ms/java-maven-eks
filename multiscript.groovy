0341e3c41805625b259e467e548facf6c1ad0e21
kalaimani-ms <108936435+kalaimani-ms@users.noreply.github.com>
2022-08-30 12:32:49 +0530

    Create multiscript.groovy

---
+++ multiscript.groovy
@@ -0,0 +1,29 @@
+def gv
+
+def Testapp() {
+        echo "Testing the application branch $BRANCH_NAME.."
+        sh 'mvn test'
+    }
+
+def Buildjar() {
+    echo 'building the maven application..'
+    sh 'mvn package'
+    }
+    
+def Buildimage() {
+    echo 'building the maven application Image..'
+    withCredentials([usernamePassword(credentialsId: 'kalaimanims-Dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker images'
+    sh 'docker build -t kalaimanims/mavenapp:1.3 .'
+    sh 'docker images'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push kalaimanims/mavenapp:1.3'
+    sh 'docker images'
+}
+}
+
+def Deployapp() {
+       echo 'deploying the application'
+    }
+
+return this
