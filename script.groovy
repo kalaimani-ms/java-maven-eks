5cff2fddbf79f558c7a2501eb7972c3a5965a63a
kalaimani-ms <kalaimanimsd@gmail.com>
2023-05-11 16:23:34 +0530

    Branch NEW-CICD is Added for Testing

--- script.groovy
+++ script.groovy
@@ -8,12 +8,12 @@ def Buildapp() {
     
 def Testapp() {
         echo 'Testing the application..'
-        echo  "testing the app version ${params.VERSION}"
+        echo  "testing the application version ${params.VERSION}"
     }
 
 def Deployapp() {
         echo 'deploying the application'
-        echo  "deploying the app version ${params.VERSION}"
+        echo  "deploying the application version ${params.VERSION}"
     }
 
 return this
